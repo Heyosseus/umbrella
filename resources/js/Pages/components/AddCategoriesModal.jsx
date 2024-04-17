674404433a75f6f54c990c0bc784712af561d056
heyosseus <ratiruxadzee@gmail.com>
2024-04-17 15:06:28 +0400

    feat: add input validations

--- resources/js/Pages/components/AddCategoriesModal.jsx
+++ resources/js/Pages/components/AddCategoriesModal.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import Modal from '@/Components/Modal.jsx';
-
+import { useForm } from 'react-hook-form';
 function AddCategoriesModal() {
     const [isOpen, setIsOpen] = useState(false);
     const [categoryName, setCategoryName] = useState('');
+    const { register, handleSubmit, setValue, getValues, formState: { errors } } = useForm();
+    const addCategories = (formData) => {
+        formData.categories = formData.categories || '';
 
-    const addCategories = (e) => {
-        e.preventDefault();
-
-        axios.post('/categories', {name: categoryName}).then((res) => {
+        axios.post('/categories', formData).then((res) => {
             console.log(res.data);
             setIsOpen(false);
         }).catch((err) => {
@@ -24,11 +24,18 @@ function AddCategoriesModal() {
             <Modal show={isOpen} onClose={() => setIsOpen(false)} maxWidth="md">
                 <div className="p-6">
                     <h1 className="text-2xl mb-4 font-semibold ">Add Categories</h1>
-                    <form onSubmit={addCategories} encType="multipart/form-data">
+                    <form onSubmit={handleSubmit(addCategories)} encType="multipart/form-data">
                         <label htmlFor="">Category name:</label>
-                        <input type="text"
-                               onChange={(e) => setCategoryName(e.target.value)}
-                               className="w-full px-3 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"/>
+                        <input
+                            type="text"
+                            placeholder="Name"
+                            {...register('name', { required: true })}
+                            className={`w-full px-3 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200 ${
+                                errors.name ? 'border-red-500' : 'border-gray-300'
+                            }`}
+                        />
+                        {errors.name && <p className="text-red-500">Category name is required</p>}
+
                         <button type="submit"
                                 className="w-full mt-4 px-5 py-2 text-lg bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-200"
                         >Add
--- resources/js/Pages/components/AddProductsModal.jsx
+++ resources/js/Pages/components/AddProductsModal.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import Modal from '@/Components/Modal.jsx';
+import { useForm } from 'react-hook-form';
 
 function AddProductsModal() {
     const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +11,8 @@ function AddProductsModal() {
     const [categories, setCategories] = useState([]);
     const [selectedCategories, setSelectedCategories] = useState([]);
     const [images, setImages] = useState([]);
+    const { register, handleSubmit, setValue, getValues, formState: { errors } } = useForm();
+
 
     const handleImageChange = (e) => {
         const files = Array.from(e.target.files);
@@ -27,23 +30,23 @@ function AddProductsModal() {
         getCategories();
     }, [])
 
-    const addProduct = (e) => {
-        e.preventDefault();
+    const addProduct = (data) => {
+        const formData = new FormData();
+        formData.append('name', data.name || '');
+        formData.append('description', data.description || '');
+        formData.append('price', data.price || '');
 
-        let formData = new FormData();
-        formData.append('name', name);
-        formData.append('description', description);
-        formData.append('price', price);
         selectedCategories.forEach((category) => {
             formData.append('categories[]', category);
         });
+
         images.forEach((image) => {
             formData.append('images[]', image);
         });
 
         axios.post('/products', formData)
             .then((res) => {
-               window.location.href = '/'
+                window.location.href = '/';
             })
             .catch((err) => {
                 console.log(err);
@@ -58,22 +61,30 @@ function AddProductsModal() {
             <Modal show={isOpen} onClose={() => setIsOpen(false)} maxWidth="2xl">
                 <div className="p-6">
                     <h1 className="text-2xl mb-4 font-semibold">Add Product</h1>
-                    <form onSubmit={addProduct} encType="multipart/form-data">
+                    <form onSubmit={handleSubmit(addProduct)} encType="multipart/form-data">
                         <label htmlFor="">Name:</label>
                         <input
                             type="text"
                             placeholder="Name"
-                            onChange={(e) => setName(e.target.value)}
-                            className="w-full px-3 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                            {...register('name', { required: true })}
+                            className={`w-full px-3 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200 ${
+                                errors.name ? 'border-red-500' : 'border-gray-300'
+                            }`}
                         />
-                        <textarea
+                        {errors.name && <p className="text-red-500">Product name is required</p>}
+                        <input
+                            type="text"
                             placeholder="Description"
-                            onChange={(e) => setDescription(e.target.value)}
-                            className="w-full px-3 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
-                        ></textarea>
+                            {...register('description', { required: true })}
+                            className={`w-full px-3 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200 ${
+                                errors.name ? 'border-red-500' : 'border-gray-300'
+                            }`}
+                        />
+                        {errors.name && <p className="text-red-500">Description is required</p>}
                         <select
                             className="w-full px-3 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                             multiple={true}
+                            {...register('selectedCategories', { required: 'Please select at least one category' })}
                             value={selectedCategories}
                             onChange={(e) => setSelectedCategories(Array.from(e.target.selectedOptions, (option) => option.value))}
                         >
@@ -81,23 +92,29 @@ function AddProductsModal() {
                                 <option key={el.id} value={el.id}>{el.name}</option>
                             ))}
                         </select>
+                        {errors.selectedCategories && <span className="text-red-500">{errors.selectedCategories.message}</span>}
                         <input
                             type="number"
                             placeholder="Price"
-                            onChange={(e) => setPrice(e.target.value)}
-                            className="w-full px-3 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                            {...register('price', { required: true })}
+                            className={`w-full px-3 py-2 mt-2 border rounded-md focus:outline-none focus:ring focus:ring-blue-200 ${
+                                errors.name ? 'border-red-500' : 'border-gray-300'
+                            }`}
                         />
+                        {errors.name && <p className="text-red-500">Price is required</p>}
                         <input
                             type="file"
                             placeholder="Image"
                             accept="image/*"
                             multiple
+                            {...register('images', { required: 'Please select at least one image' })}
                             onChange={handleImageChange}
                             className="w-full px-3 py-2 mt-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                         />
+                        {errors.images && <span className="text-red-500">{errors.images.message}</span>}
                         {images.map((image, idx) => (
                             <div key={`image-${idx}`}>
-                                <img src={URL.createObjectURL(image)} alt={`image-${idx}`} className="w-40 h-40"/>
+                                <img src={URL.createObjectURL(image)} alt={`image-${idx}`} className="w-40 h-40" />
                             </div>
                         ))}
                         <button
